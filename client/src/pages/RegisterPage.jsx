d36f413ff7775d0b907e128bb5f348d85e3b2329
AP <aleksandrpolishchuk@gmail.com>
2023-02-06 23:44:32 +0700

    add useState&useDispatch for registerPage

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, {useState, useEffect} from 'react'
+import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { registerUser } from '../redux/features/auth/authSlice';
 
 export const RegisterPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const dispatch = useDispatch();
+
+  const handleSubmit = () => {
+    try {
+      dispatch(registerUser({ username, password }))
+      setPassword('');
+      setUsername('');
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <form onSubmit={e => e.preventDefault()}
     className="w-1/4 h-60 mx-auto mt-40"
@@ -9,7 +25,9 @@ export const RegisterPage = () => {
       <h1 className="text-lg text-white text-center">Регистрация</h1>
       <label className="text-xs text-gray-400">
         Username:
-        <input type="text" 
+        <input type="text"
+        value={username}
+        onchange={e => setUsername(e.target.value)}
         placeholder="Username"
         className="mt-1 text-black w-full rounded-lg bg-gray-400 border py-1 px-2 text-xs outline-none placeholder:text-gray-700"
         />
@@ -17,7 +35,9 @@ export const RegisterPage = () => {
 
       <label className="text-xs text-gray-400">
         Password:
-        <input type="password" 
+        <input type="password"
+        value={password}
+        onchange={e => setPassword(e.target.value)}
         placeholder="Password"
         className="mt-1 text-black w-full rounded-lg bg-gray-400 border py-1 px-2 text-xs outline-none placeholder:text-gray-700"
         />
@@ -26,6 +46,7 @@ export const RegisterPage = () => {
       <div className="flex gap-8 justify-center mt-4">
         <button
           type="submit"
+          onClick={handleSubmit}
           className="flex justify-center items-center text-xs bg-gray-600 text-white rounded-sm py-2 px-4"
         >
             Подтвердить
