ea6944a4a72b2126cefeb87319e303b152d8ce0e
AP <aleksandrpolishchuk@gmail.com>
2023-02-08 23:55:36 +0700

    add client register & auth

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import {Link, NavLink} from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { checkIsAuth, logout } from '../redux/features/auth/authSlice'
+import { toast } from 'react-toastify'
 
 export const Navbar = () => {
  
-    const isAuth = useSelector();
+    const isAuth = useSelector(checkIsAuth);
+    const dispatch = useDispatch();
 
     const activeStyles = {
         color: 'white',
+    }
+    const logoutHandler = () => {
+        dispatch(logout());
+        window.localStorage.removeItem('token');
+        toast('Вы вышли из системы');
     }
 
   return (
@@ -48,7 +55,11 @@ export const Navbar = () => {
         </ul>)}
 
         <div className='flex justify-center items-center bg-gray-600 text-xs text-white rounded-sm px-4 py-2'>
-            {isAuth ? (<button>Выйти</button>) : <Link to={'/login'} > Войти </Link>}
+            {isAuth ? (
+                <button onClick = {logoutHandler}>Выйти</button>
+            ) : (
+            <Link to={'/login'} > Войти </Link>
+            )}
         </div>
     </div>
   )
