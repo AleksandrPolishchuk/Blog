48e314497b68b071e48200ab885fa5fbb9ef3f31
AP <aleksandrpolishchuk@gmail.com>
2023-02-10 23:56:44 +0700

    add markup components for AddPostPage

--- client/src/pages/AddPostPage.jsx
+++ client/src/pages/AddPostPage.jsx
@@ -1,5 +1,32 @@
 import React from 'react'
 
 export const AddPostPage = () => {
-  return <div>AddPostPage</div>
+  return (
+    <form 
+      className = 'w-1/3 mx-auto py-10'
+      onSubmit={e => e.preventDefault()}
+    >
+      <label className='text-gray-300 py-2 bg-gray-600 text-xs mt-2 flex items-center justify-center border-2 boreder-dotted cursor-pointer'>
+        Прикрепить изображение:
+      <input type="file" className='hidden' />
+      </label>
+      <div className='flex object-cover py-2'>IMAGE</div>
+
+      <label className='text-xs text-white opacity-70'>
+        Заголовок поста:
+        <input type="text"
+        placeholder='Заголовок'
+        className='mt-1 text-black w-full rounded-lg bg-gray-400 border py-1 px-2 text-xs outline-none placeholder:text-gray-700'
+        />
+      </label>
+
+      <label className='text-xs text-white opacity-70'>
+        Текст поста:
+        <textarea
+          placeholder='Текст поста'
+          className='mt-1 text-black w-full rounded-lg bg-gray-400 border py-1 px-2 text-xs outline-none resize-none h-40 placeholder:text-gray-700'
+        />
+      </label>
+    </form>
+  )
 }
