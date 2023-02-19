404290935726613a3d12675ec1dd337b59f7f2d3
AP <aleksandrpolishchuk@gmail.com>
2023-02-19 23:59:30 +0700

    add login to serv-auth

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -35,7 +35,24 @@ export const register = async (req, res) => {
 // Login user
 export const login = async (req, res) => {
   try {
-  } catch (error) {}
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.json({
+        message: "Такого юзера не существует.",
+      });
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordCorrect) {
+      return res.json({
+        message: "Неверный пароль.",
+      });
+    }
+  } catch (error) {
+    res.json({ message: "Ошибка при авторизации." });
+  }
 };
 // Get me
 export const getme = async (req, res) => {
