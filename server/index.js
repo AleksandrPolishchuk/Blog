c1ec58422275eddcb086d165a04a98e3cb6bf4cb
AP <aleksandrpolishchuk@gmail.com>
2023-02-19 23:12:44 +0700

    add base server config

--- server/index.js
+++ server/index.js
@@ -1,8 +1,33 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
 import fileUpload from "express-fileupload";
 
 const app = express();
+dotenv.config();
 
-app.listen(5000, () => {
-  console.log("Server started");
+// Constants
+const PORT = process.env.PORT || 3001;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_NAME = process.env.DB_NAME;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "All is fine." });
 });
+
+async function start() {
+  try {
+    await mongoose.connect(`path ${DB_USER}:${DB_PASSWORD} ${DB_NAME}`);
+
+    app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+}
+start();
