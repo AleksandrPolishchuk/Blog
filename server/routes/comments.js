25dcf6d59322460b9fa2fcd5e600ee1d4160a1cb
AP <aleksandrpolishchuk@gmail.com>
2023-02-26 10:14:08 +0700

    add Comment-route to srv

---
+++ server/routes/comments.js
@@ -0,0 +1,7 @@
+import { Router } from "express";
+const router = new Router();
+import { checkAuth } from "../utils/checkAuth.js";
+
+// Create Comment
+// http://localhost:3002/api/comments/:id
+router.podt("/:id", checkAuth, createComment);
