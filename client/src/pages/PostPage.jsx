768b297b725acd6c199e849e63a81f999e3da3b4
AP <aleksandrpolishchuk@gmail.com>
2023-02-26 13:47:27 +0700

    add get all PostComments (srv&cli)

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -7,11 +7,18 @@ import { toast } from 'react-toasify';
 
 import axios from '../utils/axios';
 import { removePost } from '../redux/features/post/postSlice';
+import {
+  createComment,
+  getPostComments,
+} from '../redux/features/comment/commentSlice';
+import { CommentItem } from '../components/CommentItem';
 
 export const PostPage = () => {
   const [post, setPost] = useState(null);
+  const [comment, setComment] = useState('');
 
   const { user } = useSelector(state => state.auth);
+  const { comments } = useSelector((state) => state.comment)
   const navigate = useNavigate()
   const params = useParams();
   const dispatch = useDispatch();
@@ -26,6 +33,24 @@ export const PostPage = () => {
     }
   }
 
+  const handleSubmit = () => {
+    try {
+        const postId = params.id
+        dispatch(createComment({ postId, comment }))
+        setComment('')
+    } catch (error) {
+        console.log(error)
+    }
+  }
+
+  const fetchComments = useCallback(async () => {
+    try {
+        dispatch(getPostComments(params.id))
+    } catch (error) {
+        console.log(error)
+    }
+  }, [params.id, dispatch])
+
   const fetchPost = useCallback(async () => {
     const { data } = await axios.get(`/post/${params.id}`);
     setPost(data);
@@ -34,6 +59,10 @@ export const PostPage = () => {
   useEffect(() => {
     fetchPost()
   }, [fetchPost]);
+
+  useEffect(() => {
+    fetchComments()
+}, [fetchComments])
 
   if (!post) {
     return (
@@ -91,8 +120,7 @@ export const PostPage = () => {
               </button>
             </div>
 
-            {
-              user?._id === post.author && (
+            {user?._id === post.author && (
                 <div className='flex gap-3 mt-4'>
                   <button className = 'flex items-center justify-center gap-2 text-white opacity-50'>
                     <Link to={`/${params.id}/edit`}>
@@ -101,7 +129,8 @@ export const PostPage = () => {
                   </button>
                   <button
                   onClick={removePostHandler}
-                  className='flex items-center justify-center gap-2 text-white opacity-50'>
+                  className='flex items-center justify-center gap-2 text-white opacity-50'
+                  >
                     <AiFillDelete />
                   </button>
                 </div>
@@ -111,7 +140,31 @@ export const PostPage = () => {
           </div>
 
         </div>
-        <div className='w-1/3'>COMMENTS</div>
+        <div className='w-1/3 p-8 bg-gray-700 flex flex-col gap-2 rounded-sm'>
+          <form 
+            className='flex gap-2'
+            onSubmit={(e) => e.preventDefault()}
+          >
+            <input type="text"
+              value={comment}
+              onChange={(e) => setComment(e.target.value)}
+              placeholder="Comment"
+              className="text-black w-full rounded-sm bg-gray-400 border p-2 text-xs outline-none placeholder:text-gray-700" 
+            />
+            <button
+              type='submit'
+              onClick={handleSubmit}
+              className='flex justify-center items-center bg-gray-600 text-xs text-white rounded-sm py-2 px-4'
+            >
+              Отправить
+            </button>
+          </form>
+
+          {comments?.map((cmt) => (
+            <CommentItem key={cmt._id} cmt={cmt} />
+          ))}
+
+        </div>
       </div>
     </div>
   )
