a3633f9132834f76027049c742c68cccc055bbab
AleksandrPolishchuk <aleksandrpolishchuk@gmail.com>
2023-12-15 22:54:25 -0300

    fix server index

--- server/index.js
+++ server/index.js
@@ -19,23 +19,20 @@ const DB_NAME = process.env.DB_NAME;
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(fileUpload());
 app.use(express.json());
 app.use(express.static("uploads"));
 
 // Routes
+// http://localhost:3002
 app.use("/api/auth", authRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/comments", commentRoute);
 
-app.get("/", (req, res) => {
-  return res.json({ message: "All is fine." });
-});
-
 async function start() {
   try {
     await mongoose.connect(
-      `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.q7q7yue.mongodb.net/${DB_NAME}`
+      `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.pbuqiqy.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`
     );
 
     app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
