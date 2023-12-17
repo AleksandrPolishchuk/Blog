a70f86af1c85f4035bb9ab482cf44551fc3d6740
AleksandrPolishchuk <aleksandrpolishchuk@gmail.com>
2023-12-16 22:20:53 -0300

    fix redux auth authSlice

--- client/src/redux/features/auth/authSlice.js
+++ client/src/redux/features/auth/authSlice.js
@@ -76,7 +76,7 @@ export const authSlice = createSlice({
       state.user = action.payload.user;
       state.token = action.payload.token;
     },
-    [registerUser.rejected]: (state, action) => {
+    [registerUser.rejectWithValue]: (state, action) => {
       state.status = action.payload.message;
       state.isLoading = false;
     },
@@ -91,7 +91,7 @@ export const authSlice = createSlice({
       state.user = action.payload.user;
       state.token = action.payload.token;
     },
-    [loginUser.rejected]: (state, action) => {
+    [loginUser.rejectWithValue]: (state, action) => {
       state.status = action.payload.message;
       state.isLoading = false;
     },
@@ -106,7 +106,7 @@ export const authSlice = createSlice({
       state.user = action.payload?.user;
       state.token = action.payload?.token;
     },
-    [getMe.rejected]: (state, action) => {
+    [getMe.rejectWithValue]: (state, action) => {
       state.status = action.payload.message;
       state.isLoading = false;
     },
